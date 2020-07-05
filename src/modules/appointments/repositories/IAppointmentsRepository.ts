51f9b5694f83530bffd3486132211e19a836fa8c
Everton <ton.oliveira@sempre.facig.edu.br>
2020-07-05 13:59:34 -0300

    Reescrevendo repositório

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,7 @@
-import Appointments from '../typeorm/entities/Appointments';
-
 import Appointment from '../infra/typeorm/entities/Appointments';
+import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 
 export default interface IAppointmentsRepository {
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
